4bdd8b90b97ff88046c0b7b776a0e7e4ecc8cc5d
Danny <windate3411@gmail.com>
2019-06-18 12:21:28 +0800

    add mongoDB controll

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const chalk = require('chalk')
+
+mongoose.connect('mongodb://127.0.0.1/todo', { useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('MangoDB failed');
+})
+
+db.once('open', () => {
+  console.log("MongoDB connected");
+})
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
 app.listen(3000, () => {
-  console.log('you are now listening at port 3000');
+  console.log(chalk.green.bold('you are now listening at port 3000'));
 })
